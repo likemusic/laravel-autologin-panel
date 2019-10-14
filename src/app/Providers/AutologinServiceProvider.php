60903c5f82dd5a7b77658f51cf13a2c6f50d045b
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-10-14 22:36:57 +0300

    AutologinServiceProvider fixes.

--- src/app/Providers/AutologinServiceProvider.php
+++ src/app/Providers/AutologinServiceProvider.php
@@ -18,13 +18,30 @@ class AutologinServiceProvider extends ServiceProvider
         $this->pluginBaseDir = realpath(__DIR__ . '/../../');
     }
 
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            $this->pathTo('config/autologin.php'), 'autologin'
+        );
+    }
+
     public function boot()
     {
         $this->loadRoutesFrom($this->pathTo('routes/web.php'));
-        $this->loadViewsFrom($this->pathTo('resources/views'), 'todolist');
+        $this->loadViewsFrom($this->pathTo('resources/views'), 'autologin');
         $this->publishes([
-            $this->pathTo('resources/views') => base_path('resources/views/wisdmlabs/todolist'),
-        ]);
+            $this->pathTo('config/autologin.php') => config_path('autologin.php'),
+        ], 'config');
+
+        $this->publishes([
+            $this->pathTo('resources/views') => resource_path('views/likemusic/laravel-autologin-panel'),
+        ], 'view');
+
+        $this->publishes([
+            $this->pathTo('public/js/autologin.js') => public_path('js/autologin.js'),
+            $this->pathTo('public/css/autologin.css') => public_path('css/autologin.js'),
+            $this->pathTo('public/images/avatar_man.svg') => public_path('images/avatar_man.svg'),
+        ], 'public');
     }
 
     private function pathTo($path)
